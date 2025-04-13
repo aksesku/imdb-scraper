85a483e1cbe26f68471b55b761ba86e8c17a2530
aksesku <serlinurjannah12@gmail.com>
2025-04-13 23:20:38 +0700

    download.js

---
+++ pages/api/download.js
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+  res.writeHead(302, {
+    Location: "https://incredibleenhancementslightning.com/qqumqvsjc?key=2ae095dee8d04ef97af9cb410fbb38fe",
+  });
+  res.end();
+}
