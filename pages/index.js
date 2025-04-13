6800f75fcc75ba1c1e63733f27df50920f7e05af
aksesku <serlinurjannah12@gmail.com>
2025-04-13 22:16:49 +0700

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to IMDb Scraper</h1>
+      <p>Try visiting: <code>/movie/tt5311514</code></p>
+    </div>
+  )
+}
